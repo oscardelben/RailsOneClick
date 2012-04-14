43f2fbd411505d082217b339f1940c3f40638310
Oscar Del Ben <info@oscardelben.com>
2012-04-14 14:46:24 +0200

    Add install directory structure script

---
+++ scripts/install_directory_structure.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+target_dir=~/Documents/rails_one_click
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -d $target_dir ]
+then
+  mkdir $target_dir
+  cp $script_dir/../templates/* $target_dir
+fi
---
+++ templates/readme.txt
@@ -0,0 +1,4 @@
+This directory contains all the projects that you created with
+RailsOneClick.
+
+These projects are intended to be opened with the RailsOneClick app.
